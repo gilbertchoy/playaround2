9b29166edfe6387fca6521676469f6ed2faef98b
bort ske <gilbertchoy@gmail.com>
2020-11-15 21:38:20 -0800

    sorting

--- app/src/main/java/com/bort/playaround2/MainActivity.java
+++ app/src/main/java/com/bort/playaround2/MainActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
     String tag = "berttest output";
     int ar[]=new int[7];  //mergeSort
+    int arr[]=new int[7];  //quickSort
+    int array[] = new int[7];  //heapSort
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +24,52 @@ public class MainActivity extends AppCompatActivity {
         ar[4]=2;
         ar[5]=6;
         ar[6]=7;
-        mergeSort(ar,0,ar.length);
+        mergeSort(ar,0,ar.length-1);
         Log.d(tag,"mergesort:");
         printValues(ar);
 
+        //quickSort
+        arr[0]=4;
+        arr[1]=5;
+        arr[2]=3;
+        arr[3]=1;
+        arr[4]=2;
+        arr[5]=6;
+        arr[6]=7;
+        mergeSort(ar,0,ar.length-1);
+        Log.d(tag,"quickSort:");
+        printValues(ar);
+
+
+        array[0]=4;
+        array[1]=5;
+        array[2]=3;
+        array[3]=1;
+        array[4]=2;
+        array[5]=6;
+        array[6]=7;
+
 
     }
+
+    public static void quickSort(int[] a, int l, int r){
+        int pi = partition(a,l,r);
+        quickSort(a,l,pi-1);
+        quickSort(a,pi+1,r);
+    }
+
+    public static int partition(int[] a, int l, int r){
+        int incrementi;
+        int swapi;
+
+        for()
+
+
+        return 1;
+    }
+
+
+
 
     public static void mergeSort(int [] a, int l, int r){
         //split then merge
@@ -54,7 +96,6 @@ public class MainActivity extends AppCompatActivity {
         int lc = 0;
         int rc = 0;
         int index = l;
-
         while(lc<left.length && rc<right.length){
             if(left[lc]<right[rc]){
                 a[index] = left[lc];
@@ -66,21 +107,16 @@ public class MainActivity extends AppCompatActivity {
             }
             index++;
         }
-
         while(lc<left.length){
             a[index] = left[lc];
             lc++;
             index++;
         }
-
         while(rc<right.length){
             a[index] = right[rc];
             rc++;
             index++;
         }
-
-
-
     }
 
 
