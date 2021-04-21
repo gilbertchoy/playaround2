1a188dfdfe3427c1c0c67dafc0be2993cc2c2a09
bort ske <gilbertchoy@gmail.com>
2021-04-21 14:13:18 -0700

    heapsort

--- app/src/main/java/com/bort/playaround2/MainActivity.java
+++ app/src/main/java/com/bort/playaround2/MainActivity.java
@@ -28,6 +28,16 @@ public class MainActivity extends AppCompatActivity {
         Log.d(tag,"mergesort:");
         printValues(ar);
 
+        int ar1[] = new int[5];
+        ar1[0]=4;
+        ar1[1]=5;
+        ar1[2]=3;
+        ar1[3]=1;
+        ar1[4]=2;
+        mergeSort(ar1, 0, ar1.length-1);
+        Log.d(tag, "mergesort1:");
+        printValues(ar1);
+
         //quickSort
         arr[0]=4;
         arr[1]=5;
@@ -36,11 +46,11 @@ public class MainActivity extends AppCompatActivity {
         arr[4]=2;
         arr[5]=6;
         arr[6]=7;
-        mergeSort(ar,0,ar.length-1);
+        mergeSort(arr,0,ar.length-1);
         Log.d(tag,"quickSort:");
-        printValues(ar);
+        printValues(arr);
 
-
+        //heapSort
         array[0]=4;
         array[1]=5;
         array[2]=3;
@@ -48,7 +58,13 @@ public class MainActivity extends AppCompatActivity {
         array[4]=2;
         array[5]=6;
         array[6]=7;
+        mergeSort(array,0,ar.length-1);
+        Log.d(tag,"heapSort:");
+        printValues(array);
 
+    }
+
+    public static void heapSort(int[] a){
 
     }
 
@@ -59,17 +75,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static int partition(int[] a, int l, int r){
-        int incrementi;
-        int swapi;
+        //use last element as pivot
+        int swapi = l;  //swap index
 
-        for()
+        for(int i = l; i<r; i++){
+            if(a[i] <= a[r]){
+                //nothing
+                int swap = a[i];
+                a[i] = a[swapi];
+                a[swapi] = swap;
+                swapi++;
+            }
+        }
 
+        //swap last and swapi
+        int swap = a[swapi];
+        a[swapi] = a[r];
+        a[r] = swap;
 
-        return 1;
+        return swapi;
     }
-
-
-
 
     public static void mergeSort(int [] a, int l, int r){
         //split then merge
@@ -118,6 +143,70 @@ public class MainActivity extends AppCompatActivity {
             index++;
         }
     }
+
+
+//    public static void mergeSort1(int[] a, int l, int r){
+//        if(l<r){
+//            int m = (l+r)/2;
+//            mergeSort1(a,l,m);
+//            mergeSort1(a,m+1, r);
+//            merge1(a,l,m,r);
+//        }
+//    }
+//
+//    public static void merge1(int[] a, int l, int m, int r){
+//        int[] left = new int[m-l+1];
+//        int[] right = new int[r-m];
+//
+//        for(int i=0; i<left.length; i++){
+//            left[i] = a[l+i];
+//        }
+//
+//
+//
+//
+//    }
+
+    static void heapSort(int[] a){
+        int n = a.length;
+
+        for(int i = n/2-1; i>=0; i--){
+            heapify(a,n,i);
+        }
+
+        for(int i=n-1; i>0; i--){  //last var
+            int temp = a[0];
+            a[0] = a[i];
+            a[i]=temp;
+
+            heapify(a,i,0);
+        }
+    }
+
+    static void heapify(int[] a, int n, int i){ //n is length of array (length not including sorted part), i is the root
+        int largest = i;
+        int l = 2*i +1;
+        int r = 2*i +2;
+
+        if(l<n && a[l] > a[largest]){
+            largest = l;
+        }
+        if(r<n && a[r] > a[largest]){
+            largest = r;
+        }
+
+        if(largest !=i){
+            int swap = a[largest];
+            a[largest] = a[i];
+            a[i] = a[largest];
+
+            heapify(a,n,largest);
+        }
+
+    }
+
+
+
 
 
     public void printValues(int a[]){
